57fd299d1a97c9a0c54ce8db88c557cbe0930894
yassermembreno <yass.membreo.gudiel@gmail.com>
2021-06-10 12:07:31 -0600

    Added BinaryReaderExtension

---
+++ Infraestructure/Data/BinaryReaderExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Data
+{
+    public static class BinaryReaderExtension
+    {
+        public static object GetValue<T>(this BinaryReader br, Type type)
+        {
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Boolean: { return br.ReadBoolean(); }
+                case TypeCode.Byte: { return br.ReadByte(); }
+                case TypeCode.Char: { return br.ReadChar(); }
+                case TypeCode.Decimal: { return br.ReadDecimal(); }
+                case TypeCode.Double: { return br.ReadDouble(); }
+                case TypeCode.Single: { return br.ReadSingle(); }
+                case TypeCode.Int16: { return br.ReadInt16(); }
+                case TypeCode.Int32: { return br.ReadInt32(); }
+                case TypeCode.Int64: { return br.ReadUInt64(); }
+                default: { return br.Read(); }
+            }
+        }
+    }
+}
--- Infraestructure/Data/RAFContext.cs
+++ Infraestructure/Data/RAFContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +25,43 @@ namespace Infraestructure.Data
         public Stream DataStream
         {
             get => File.Open($"{fileName}.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        }
+
+        public void Create<T>(T t)
+        {
+            using (BinaryWriter bwHeader = new BinaryWriter(HeaderStream),
+                                  bwData = new BinaryWriter(DataStream))
+            {
+                int n, k;
+                using (BinaryReader brHeader = new BinaryReader(bwHeader.BaseStream))
+                {
+                    if (brHeader.BaseStream.Length == 0)
+                    {
+                        n = 0;
+                        k = 0;
+                    }
+                    else
+                    {
+                        brHeader.BaseStream.Seek(0, SeekOrigin.Begin);
+                        n = brHeader.ReadInt32();
+                        k = brHeader.ReadInt32();
+                    }
+
+                    PropertyInfo[] info = t.GetType().GetProperties();
+                    foreach (PropertyInfo pinfo in info)
+                    {
+                        bwData.Write(pinfo);
+                    }
+
+                    long posh = 8 + n * 4;
+                    bwHeader.BaseStream.Seek(posh, SeekOrigin.Begin);
+                    bwHeader.Write(k);
+
+                    bwHeader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    bwHeader.Write(++n);
+                    bwHeader.Write(k);
+                }
+            }
         }
     }
 }
